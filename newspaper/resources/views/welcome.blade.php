a90fc948c53d63b9593d04af1246c44d34c75a57
Jimmy Svensson <jimmy888swe@gmail.com>
2019-05-13 13:25:07 +0200

    secound

--- newspaper/resources/views/welcome.blade.php
+++ newspaper/resources/views/welcome.blade.php
@@ -64,34 +64,21 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
+     
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Fantasy Sport News
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Quidditch</a>
+                    <a href="https://laracasts.com">Lightsaber Fencing</a>
+                    <a href="https://laravel-news.com">Dejarik</a>
+                    <a href="https://blog.laravel.com">Dragon Dressage</a>
+                    <a href="https://nova.laravel.com">Fot-Boule</a>
+                    <a href="https://forge.laravel.com">Sewer Surfin</a>
+                    <a href="https://github.com/laravel/laravel">Anbo-Jitsu</a>
+                    <a href="https://github.com/laravel/laravel">Blernsball</a>
                 </div>
             </div>
         </div>
