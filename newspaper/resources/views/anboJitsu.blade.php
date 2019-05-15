6a6951ac02b118d1b6ddc10347911bc2a22747a5
Henrik Foogel <henrik.foogel@medieinstitutet.se>
2019-05-15 15:29:08 +0200

    something

--- newspaper/resources/views/anboJitsu.blade.php
+++ newspaper/resources/views/anboJitsu.blade.php
@@ -1,21 +1,20 @@
 @extends('articles')
 @extends('header')
-    
+
 @section('main')
 <div>
-    
+
     <h1>Anbo-Jitsu</h1>
     @if (count($anboJitsu) > 0)
 
-    @foreach($anboJitsu as $anbo) 
-    <li><a href="{!! url('articles/'.$anbo->id) !!}" class="articlelink"><div class="front-page-article-headline">{!! $anbo->headline !!}</div><span class="front-page-article-text">{!! substr($anbo->text, 0, 200) !!}...</span></a></li>
-    <span><img style="" src="{{ $article->img }}"></span>
+    @foreach($anboJitsu as $anbo)
+    <li><a href="{!! url('articles/'.$anbo->id) !!}" class="articlelink">
+            <div class="front-page-article-headline">{!! $anbo->headline !!}</div><span class="front-page-article-text">{!! substr($anbo->text, 0, 200) !!}...</span>
+        </a></li>
 
-
-    
     @endforeach
     @endif
 
-    </div>
+</div>
 
 @endsection
