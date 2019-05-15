49c1deac60e7e5d7ad3dc32bef687f7f18528e0c
Jimmy Svensson <jimmy888swe@gmail.com>
2019-05-15 19:06:25 +0200

    imgPrevOnAllCatPage

--- newspaper/resources/views/sewerSurfin.blade.php
+++ newspaper/resources/views/sewerSurfin.blade.php
@@ -5,12 +5,15 @@
 <div>
     <h1>Sewer Surfin</h1>
     @if (count($sewers) > 0)
-
+    <div class="single-article-div">
     @foreach($sewers as $sewer) 
     <li><a href="{!! url('articles/'.$sewer->id) !!}" class="articlelink"><div class="front-page-article-headline">{!! $sewer->headline !!}</div><span class="front-page-article-text">{!! substr($sewer->text, 0, 200) !!}...</span></a></li>
+    <span style="width: 100%; height:5rem; overflow:hidden;"><img src="{{ $sewer->img }}"></span>
 
     
     @endforeach
+    </div>
+
     @endif
 
     </div>
